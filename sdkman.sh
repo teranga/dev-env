b84136aa0384a201bade20de17a26714deb6549f
Pol Bassiner <pbassiner@netquest.com>
2016-12-06 18:11:31 +0100

    fixup! use sdkman to install java 8

--- sdkman.sh
+++ sdkman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export sdkman_beta_channel=true
+#export sdkman_beta_channel=true
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate force
 sdk flush candidates
-sdk install java 8u111
+echo "Y" | sdk install java
 sdk install sbt
 sdk install maven
