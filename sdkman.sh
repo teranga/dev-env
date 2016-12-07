713f3b1efdd2d8aada270f9adcd8340674bf1109
Pol Bassiner <pbassiner@netquest.com>
2016-12-07 09:11:34 +0100

    fixup! use sdkman to install java 8

--- sdkman.sh
+++ sdkman.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-#export sdkman_beta_channel=true
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate force
 sdk flush candidates
-echo "Y" | sdk install java
+
+# Hack to avoid using cat with /dev/random, while waiting for PR to be merged
+sed -i 's/cat \/dev\/urandom/head \/dev\/urandom/g' /$HOME/.sdkman/src/sdkman-install.sh
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+echo "Y" | sdk install java # Accept license
 sdk install sbt
 sdk install maven
